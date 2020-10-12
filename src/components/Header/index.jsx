1717c2ca011fbe5d80d75947ec1c002c7d316d17
Citentel <gontrebor@gmail.com>
2020-10-13 01:52:48 +0200

    add text header into content folder

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import './static/css/index.css'
 
-const Header = (props) => (
-    <header className='header'>
-        <div className='header__box'>
-            <h1 className='header__box--h1'>{props.h1}</h1>
-            <p className='header__box--p'>{props.p}</p>
-        </div>
-    </header>
-)
+const Header = (props) => {
+    const content = require('./static/content/text').default[props.type]
+
+    return(
+        <header className='header'>
+            <div className='header__box'>
+                <h1 className='header__box--h1'>{content.h1}</h1>
+                <p className='header__box--p'>{content.p}</p>
+            </div>
+        </header>
+    )
+}
+
+
 
 export default Header
---
+++ src/components/Header/static/content/text.jsx
@@ -0,0 +1,14 @@
+export default {
+    basic : {
+        h1 : "To ja Robert",
+        p : "Web deweloper"
+    },
+    project : {
+        h1 : "Moja praca",
+        p : "Wybrane moje projekty"
+    },
+    notFound : {
+        h1 : "404",
+        p : "Coś poszło nie tak..."
+    }
+}
