7351d1ec53ab22031abf8c0730a4f8c9f6d5b836
Citentel <gontrebor@gmail.com>
2020-10-13 01:37:48 +0200

    change file type to jsx

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import Loader from './components/Loader'
+import Loader from './components/Loader/index'
 
-const App = React.lazy(() => import('./App'))
+const App = React.lazy(() => import('./App.jsx'))
 
 ReactDOM.render(
     <Suspense fallback={<Loader />}>
